cad58bf786c13c194d59edb22316a5c719dea75e
Wojciech Lubojanski <lubojanski.wojciech@gmail.com>
2022-11-14 20:43:50 +0100

    add head to article, add padding  for mobile

--- pages/posts/[...slug].tsx
+++ pages/posts/[...slug].tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import { ParsedUrlQuery } from "querystring";
 import { Post } from "../../types";
@@ -39,53 +40,60 @@ export const getServerSideProps: GetServerSideProps<PageProps> = async (
 const PostPage: NextPage<PageProps> = (props) => {
   const { post } = props;
   return (
-    <div
-      className="justify-start 
+    <>
+      <Head>
+        <title>Next.js blog - article </title>
+        <meta name="description" content="A homework project" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        className="justify-start p-4
 max-w-3xl mx-auto group hover:no-underline focus:no-underline  relative overflow-hidden object-cover flex flex-col  "
-    >
-      <div className="relative min-h-[288px] w-full object-contain">
-        <Image
-          priority
-          data-cy-testid="post-image"
-          className=" rounded-b-none h-72 object-cover"
-          src={post?.imageUrl ?? ""}
-          alt={`blog post image - ${post?.title}`}
-          fill
-          sizes="100vw"
-        />
-      </div>
-      <div className=" pt-6 space-y-3">
-        <div className="flex space-x-6 text-indigo-600 text-sm">
-          {post?.categories?.map((category) => (
-            <div key={category} data-cy-testid="post-category">
-              {category}
-            </div>
-          ))}
+      >
+        <div className="relative min-h-[288px] w-full object-contain">
+          <Image
+            priority
+            data-cy-testid="post-image"
+            className=" rounded-b-none h-72 object-cover"
+            src={post?.imageUrl ?? ""}
+            alt={`blog post image - ${post?.title}`}
+            fill
+            sizes="100vw"
+          />
         </div>
-        <h3
-          className="text-2xl font-semibold tracking-wide line-clamp-2 "
-          data-cy-testid="post-title"
-        >
-          {post?.title}
-        </h3>
-        <p
-          className="text-slate-500 line-clamp-3"
-          data-cy-testid="post-excerpt"
-        >
-          {post?.excerpt}
-        </p>
-      </div>
-      <div className="py-6 mb-6  flex space-x-2 text-sm mt-auto ">
-        <div className="rounded-full w-10 h-10 bg-slate-400 "></div>
-        <div className="flex flex-col">
-          <div className="font-semibold"> Author name</div>
-          <div className="text-slate-500">
-            <span>Mar 10, 2020</span>
-            <span> · 5 min read</span>
+        <div className=" pt-6 space-y-3">
+          <div className="flex space-x-6 text-indigo-600 text-sm">
+            {post?.categories?.map((category) => (
+              <div key={category} data-cy-testid="post-category">
+                {category}
+              </div>
+            ))}
+          </div>
+          <h3
+            className="text-2xl font-semibold tracking-wide line-clamp-2 "
+            data-cy-testid="post-title"
+          >
+            {post?.title}
+          </h3>
+          <p
+            className="text-slate-500 line-clamp-3"
+            data-cy-testid="post-excerpt"
+          >
+            {post?.excerpt}
+          </p>
+        </div>
+        <div className="py-6 mb-6  flex space-x-2 text-sm mt-auto ">
+          <div className="rounded-full w-10 h-10 bg-slate-400 "></div>
+          <div className="flex flex-col">
+            <div className="font-semibold"> Author name</div>
+            <div className="text-slate-500">
+              <span>Mar 10, 2020</span>
+              <span> · 5 min read</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
